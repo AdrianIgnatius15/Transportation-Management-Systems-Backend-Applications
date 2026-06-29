f8c8693c4424afd6fc30e8612742f1d9c32601d4
Adrian Joseph <adrianignatius15@hotmail.com>
2026-06-29 22:32:34 +0800

    Created a new placeholder for `Tracking` controller

---
+++ Transportation-Management-System-Tracking-Service-REST-API/Controllers/TrackingController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Transportation_Management_System_Tracking_Service_REST_API.Controllers
+{
+    public class TrackingController : ControllerBase
+    {
+        
+    }
+}
