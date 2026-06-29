7bb69e3ff722a11a0fd004f882a9ac2c8a997ae5
Adrian Joseph <adrianignatius15@hotmail.com>
2026-06-29 22:31:40 +0800

    Created new DTOs for `TrackingEvent`

---
+++ Transportation-Management-System-Tracking-Service-REST-API/DTOs/TrackingEventReadDto.cs
@@ -0,0 +1,15 @@
+using Transportation_Management_System_Tracking_Service_REST_API.Models.Enums;
+
+namespace Transportation_Management_System_Tracking_Service_REST_API.DTOs
+{
+    public class TrackingEventReadDto
+    {
+        public TrackingEventType EventType { get; set; }
+
+        public string? Description { get; set; }
+
+        public double? Latitude { get; set; }
+
+        public double? Longitude { get; set; }
+    }
+}
