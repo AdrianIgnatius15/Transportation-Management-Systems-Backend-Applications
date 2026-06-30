17b82596d79cd4b74bc7fedb038ddfb847488e8e
Adrian Joseph <adrianignatius15@hotmail.com>
2026-06-30 23:36:41 +0800

    Registered new middlewares & services

--- Transportation-Management-System-Tracking-Service-REST-API/Program.cs
+++ Transportation-Management-System-Tracking-Service-REST-API/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Transportation_Management_System_Tracking_Service_REST_API.Data;
+using Transportation_Management_System_Tracking_Service_REST_API.Data.Interfaces;
+using Transportation_Management_System_Tracking_Service_REST_API.Data.Repositories;
+using Transportation_Management_System_Tracking_Service_REST_API.Middlewares;
+using Transportation_Management_System_Tracking_Service_REST_API.Middlewares.Interfaces;
+using Transportation_Management_System_Tracking_Service_REST_API.SignalRControllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +14,15 @@ builder.Services.AddDbContext<TMSDbContext>(options => options.UseNpgsql(
     builder.Configuration.GetConnectionString("TMS-Database")
 ));
 
+// --------- Rabbit MQ Messaging Queue Middleware ------------
+builder.Services.AddSingleton<IRabbitMQConnection, RabbitMQConnectionMiddleware>();
+
+// --------- Interface contract & actual repository ----------
+builder.Services.AddScoped<ITrackingEventRepo, TrackingRepo>();
+builder.Services.AddScoped<IMQProducer, MqProducer>();
+
 builder.Services.AddControllers();
+builder.Services.AddSignalR();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -26,5 +39,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<TrackingHubController>("/hubs/tracking");
 
 app.Run();
