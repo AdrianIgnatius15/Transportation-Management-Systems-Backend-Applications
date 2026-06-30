eecf2b0578b7804a062278e1b831932347b77ee5
Adrian Joseph <adrianignatius15@hotmail.com>
2026-06-30 23:35:41 +0800

    Created new utility classes

---
+++ Transportation-Management-System-Tracking-Service-REST-API/Utilities/MapperUtility.cs
@@ -0,0 +1,29 @@
+namespace Transportation_Management_System_Tracking_Service_REST_API.Utilities
+{
+    public static class MapperUtility
+    {
+        public static TDestination Map<TSource, TDestination>(TSource source)
+            where TDestination : new()
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            TDestination destination = new TDestination();
+            var sourceProperties = typeof(TSource).GetProperties();
+            var destinationProperties = typeof(TDestination).GetProperties();
+
+            foreach (var sourceProp in sourceProperties)
+            {
+                var destProp = destinationProperties.FirstOrDefault(p => p.Name == sourceProp.Name && p.PropertyType == sourceProp.PropertyType);
+                if (destProp != null && destProp.CanWrite)
+                {
+                    destProp.SetValue(destination, sourceProp.GetValue(source));
+                }
+            }
+
+            return destination;
+        }
+    }
+}
