e87a162289fb840ce77974df5291f93d044a2b88
Adrian Joseph <adrianignatius15@hotmail.com>
2026-09-13 11:00:13 +0800

    Created new service REST API using Kotlin SpringBoot called "Document Service REST API"

---
+++ Transportation-Management-Systems-Document-Service-REST-API/document-service-rest-api/src/test/kotlin/com/example/tms/document_service_rest_api/DocumentServiceRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tms.document_service_rest_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DocumentServiceRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
